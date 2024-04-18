994ae61ce76daddec7bb32f44c5850190075b644
Alex Oliveira <alexoliveira7x@gmail.com>
2024-04-18 14:04:03 +0100

    fix: update assoation

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -6,21 +6,53 @@ import OrderModel from "./order.model";
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async update(entity: Order): Promise<void> {
-    await OrderModel.update(
-      {
-        customer_id: entity.customerId,
-        total: entity.total(),
-        items: entity.items.map((item) => ({
-          id: item.id,
-          name: item.name,
-          price: item.price,
-          product_id: item.productId,
-          quantity: item.quantity,
-        })),
-      },
-      { where: { id: entity.id } },
-    );
+    let transaction;
+    try {
+      transaction = await OrderModel.sequelize.transaction();
+
+      await OrderModel.update(
+        {
+          customer_id: entity.customerId,
+          total: entity.total(),
+        },
+        {
+          where: { id: entity.id },
+          transaction,
+        },
+      );
+
+      // This part is because of issue about updating associations
+      // https://github.com/sequelize/sequelize-typescript/issues/309
+
+      // Given the context: https://forum.code.education/forum/topico/desafio-implementacao-do-orderrespository-ddd-modelagem-tatica-e-patterns-1237/
+      // I really dont like the approach of delete all items and insert again with bulkCreate
+      // If we are not using DELETE method, maybe a solution can be near to this: https://api.rubyonrails.org/v7.1.3/classes/ActiveRecord/NestedAttributes/ClassMethods.html
+      // My suggestion here is run a loop that create or update, and consider the _destroy flag to delete the item, i will not implement this here.
+      await OrderItemModel.destroy({
+        where: { order_id: entity.id },
+        transaction,
+      });
+      const items = entity.items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity,
+        order_id: entity.id,
+      }));
+      await OrderItemModel.bulkCreate(items, { transaction });
+      await OrderModel.update(
+        { total: entity.total() },
+        { where: { id: entity.id }, transaction },
+      );
+
+      await transaction.commit();
+    } catch (error) {
+      if (transaction) await transaction.rollback();
+      throw error;
+    }
   }
+
   async find(id: string): Promise<Order> {
     const order = await OrderModel.findByPk(id, {
       include: [{ model: OrderItemModel }],
